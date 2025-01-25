4af7f4e66ec7c2ca6b651708a7943195ab3e8f0d
Jakub Kopřiva <kopriva.jakub@gmail.com>
2025-01-25 12:59:21 +0100

    input system

---
+++ Assets/Scripts/InputSystem/PlayerInputController.cs
@@ -0,0 +1,79 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerInputController : MonoBehaviour
+{
+    [SerializeField] PlayerInput playerInput;
+    [SerializeField] PlayerController playerController;
+    InputAction forward;
+    InputAction backward;
+    InputAction left;
+    InputAction right;
+    InputAction shoot;
+    InputAction dive;
+    InputAction jump;
+    Vector4 movementVector = new Vector4();
+
+    public void Start()
+    {
+        var input = new UnityInputSystemActions();
+        // Not ideal, I would do it over an Unity event as recommended in https://docs.unity3d.com/Packages/com.unity.inputsystem@1.5/manual/ActionAssets.html
+        forward = playerInput.actions.FindAction(input.Player.Forward.id);
+        backward = playerInput.actions. FindAction(input.Player.Backward.id);
+        left = playerInput.actions.FindAction(input.Player.Left.id);
+        right = playerInput.actions.FindAction(input.Player.Right.id);
+        shoot = playerInput.actions.FindAction(input.Player.Shoot.id);
+        dive = playerInput.actions.FindAction(input.Player.Dive.id);
+        jump = playerInput.actions.FindAction(input.Player.Jump.id);
+    }
+
+    public void Update()
+    {
+        var fwd = forward.ReadValue<float>();
+        var bck = backward.ReadValue<float>();
+        var lft = left.ReadValue<float>();
+        var rgt = right.ReadValue<float>();
+        var sht = shoot.ReadValue<float>();
+        var dv = dive.ReadValue<float>();
+        var j = jump.ReadValue<float>();
+        
+        playerController.SetInputVector(new Vector4(fwd, bck, lft, rgt));
+    }
+
+    public void OnForward(InputAction.CallbackContext context)
+    {
+        Debug.Log($"Forward pressed ({gameObject.name}): {context.ReadValueAsButton()}");
+    }
+
+    public void OnBackward(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnLeft(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnRight(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnDive(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/InputSystem/PlayerInputSystemManager.cs
@@ -0,0 +1,94 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+
+[RequireComponent(typeof(PlayerInputManager))]
+public class PlayerInputSystemManager : MonoBehaviour
+{
+    private static class ControlScheme
+    {
+        public const string KEYBOARD_1 = "Keyboard1";
+        public const string KEYBOARD_2 = "Keyboard2";
+        public const string KEYBOARD_3 = "Keyboard3";
+        public const string KEYBOARD_4 = "Keyboard4";
+        public const string GAMEPAD = "Gamepad";
+    }
+
+    [SerializeField] PlayerInputManager playerInputManager;
+    HashSet<KeyValuePair<InputDevice, string>> pairedDevices = new ();
+    [SerializeField] GameObject playerPrefab;
+
+    void Start()
+    {
+        playerInputManager.onPlayerJoined += OnPlayerJoined;
+        playerInputManager.playerPrefab = playerPrefab;
+        Debug.Log($"Joining enabled: {playerInputManager.joiningEnabled}");
+    }
+
+    void OnDestroy()
+    {
+        playerInputManager.onPlayerJoined -= OnPlayerJoined;
+    }
+
+    private void OnPlayerJoined(PlayerInput playerInput)
+    {
+        if (playerInputManager.playerCount >= playerInputManager.maxPlayerCount)
+        {
+            playerInputManager.DisableJoining();
+        }
+        Debug.Log($"On player joined: n: {playerInput.gameObject.name}, s: {playerInput.currentControlScheme}");
+        Debug.Log($"Joining enabled: {playerInputManager.joiningEnabled}");
+    }
+
+    void Update()
+    {
+        if (!playerInputManager.joiningEnabled)
+        {
+            return;
+        }
+        if (Keyboard.current[Key.LeftCtrl].wasPressedThisFrame && !IsDevicePaired(Keyboard.current, ControlScheme.KEYBOARD_1))
+        {
+            JoinPlayer(Keyboard.current, ControlScheme.KEYBOARD_1);
+        }   
+        if (Keyboard.current[Key.RightMeta].wasPressedThisFrame && !IsDevicePaired(Keyboard.current, ControlScheme.KEYBOARD_2))
+        {
+            JoinPlayer(Keyboard.current, ControlScheme.KEYBOARD_2);
+        }   
+        if (Keyboard.current[Key.Delete].wasPressedThisFrame && !IsDevicePaired(Keyboard.current, ControlScheme.KEYBOARD_3))
+        {
+            JoinPlayer(Keyboard.current, ControlScheme.KEYBOARD_3);
+        }   
+        if (Keyboard.current[Key.NumpadEnter].wasPressedThisFrame && !IsDevicePaired(Keyboard.current, ControlScheme.KEYBOARD_4))
+        {
+            JoinPlayer(Keyboard.current, ControlScheme.KEYBOARD_4);
+        }
+        
+        var gamepads = Gamepad.all;
+        foreach (var g in gamepads)
+        {
+            if (
+                g[GamepadButton.South].wasPressedThisFrame &&
+                !IsDevicePaired(g, ControlScheme.GAMEPAD)
+            ) {
+                JoinPlayer(g, ControlScheme.GAMEPAD);
+            }
+        } 
+    }
+    bool IsDevicePaired(InputDevice inputDevice, string controlScheme)
+    {
+        return pairedDevices.Contains(new KeyValuePair<InputDevice, string>(inputDevice, controlScheme));
+    }
+
+    void JoinPlayer(InputDevice inputDevice, string controlScheme)
+    {
+        pairedDevices.Add(new KeyValuePair<InputDevice, string>(inputDevice, controlScheme));
+
+        playerInputManager.JoinPlayer(
+            playerInputManager.playerCount,
+            -1,
+            controlScheme,
+            inputDevice
+        );
+    }
+}
