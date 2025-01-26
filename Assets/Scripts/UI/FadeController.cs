f48e09964e612687a48f28ad27c8028727e590ce
Jan Slifka <jan.slifka@gmail.com>
2025-01-26 11:24:09 +0100

    Fix event system and improve buttons

--- Assets/Scripts/UI/FadeController.cs
+++ Assets/Scripts/UI/FadeController.cs
@@ -22,8 +22,6 @@ public class FadeController : MonoBehaviour
             Destroy(this.gameObject);
             return;
         }
-
-        DontDestroyOnLoad(this);
     }
 
     public void FadeOut()
